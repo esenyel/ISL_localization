d49fee6cedcd9faa23b35c46b02203ac1302d613
hlkbyrm <haluk61@yahoo.com>
2015-11-24 22:42:37 +0200

    omni_invariants

--- qtcreator-build/src/database/moc_databasemanager.cxx
+++ qtcreator-build/src/database/moc_databasemanager.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'databasemanager.h'
 **
-** Created: Tue Nov 24 22:18:14 2015
+** Created: Tue Nov 24 22:30:16 2015
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
